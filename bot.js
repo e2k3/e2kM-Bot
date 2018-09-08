6adf21bd838bb40443e2500655c038c9418c3e13
e2k3 <43041483+e2k3@users.noreply.github.com>
2018-09-08 11:03:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,6 +48,26 @@ client.user.setStatus("dnd")
 
 
 
+client.on('message', message => {
+              if (!message.channel.guild) return;
+      if(message.content =='+Speicals')
+      var kayan = new Discord.RichEmbed()
+      .setThumbnail(message.author.avatarURL)
+      .setFooter(message.author.username, message.author.avatarURL) 
+      .setTitle('🌷| Members info')
+      .addBlankField(true)
+      .addField('📗| Online',
+      `${message.guild.members.filter(m=>m.presence.status == 'online').size}`)
+      .addField('📕| DND',`${message.guild.members.filter(m=>m.presence.status == 'dnd').size}`)
+      .addField('📙| Idle',`${message.guild.members.filter(m=>m.presence.status == 'idle').size}`)
+      .addField('📓| Offline',`${message.guild.members.filter(m=>m.presence.status == 'offline').size}`)
+      .addField('➡| Server Members',`${message.guild.memberCount}`)
+      message.channel.send(kayan);
+    
+    });
+
+
+
 var ss = 0;
 
 client.on('voiceStateUpdate', (o,n) => {
